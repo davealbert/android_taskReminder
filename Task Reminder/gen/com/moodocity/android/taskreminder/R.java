4508caa88b1c5d8c3e2591928964a819aee82154
Dave Albert <dave.albert@publicisdhealthcare.ie>
2012-05-08 15:58:13 +0100

    empty ListActivity

--- Task Reminder/gen/com/moodocity/android/taskreminder/R.java
+++ Task Reminder/gen/com/moodocity/android/taskreminder/R.java
@@ -14,10 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int reminder_list=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int no_reminders=0x7f040002;
     }
 }
--- Task Reminder/src/com/moodocity/android/taskreminder/ReminderListActivity.java
+++ Task Reminder/src/com/moodocity/android/taskreminder/ReminderListActivity.java
@@ -1,13 +1,14 @@
 package com.moodocity.android.taskreminder;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class ReminderListActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class ReminderListActivity extends ListActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.reminder_list);
+	}
 }
