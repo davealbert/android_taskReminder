3a4ab17b46db607dc8ef3f76d8b4cc949b467786
Dave Albert <dave.albert@publicisdhealthcare.ie>
2012-05-08 16:28:32 +0100

    Edit List started but not accessable

---
+++ Task Reminder/src/com/moodocity/android/taskreminder/ReminderEditActivity.java
@@ -0,0 +1,12 @@
+package com.moodocity.android.taskreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReminderEditActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.reminder_edit);
+	}
+}
