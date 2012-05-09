c521432110c0865f7aa70d8ef9429f613ab80f3c
Dave Albert <dave.albert@publicisdhealthcare.ie>
2012-05-09 09:21:07 +0100

    Abandond task reminder.  Book was very bad

--- Task Reminder/gen/com/moodocity/android/taskreminder/R.java
+++ Task Reminder/gen/com/moodocity/android/taskreminder/R.java
@@ -41,6 +41,7 @@ public final class R {
         public static final int menu_delete=0x7f040009;
         public static final int menu_insert=0x7f040008;
         public static final int no_reminders=0x7f040002;
+        public static final int task_saved_message=0x7f04000a;
         public static final int time=0x7f040006;
         public static final int title=0x7f040003;
     }
--- Task Reminder/src/com/moodocity/android/taskreminder/ReminderEditActivity.java
+++ Task Reminder/src/com/moodocity/android/taskreminder/ReminderEditActivity.java
@@ -11,14 +11,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class ReminderEditActivity extends Activity {
 	private Button mTimeButton;
+	private EditText mTitleText;
+	private Button mConfirmButton;
+	private EditText mBodyText;
+	private RemindersDbAdapter mDbHelper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mDbHelper = new RemindersDbAdapter(this);
+
+		mConfirmButton = (Button) findViewById(R.id.confirm);
+		mTitleText = (EditText) findViewById(R.id.title);
+		mBodyText = (EditText) findViewById(R.id.body);
+
 		setContentView(R.layout.reminder_edit);
 
 		mDateButton = (Button) findViewById(R.id.reminder_date);
@@ -39,33 +51,54 @@ public class ReminderEditActivity extends Activity {
 
 	private void registerButtonListenersAndSetDefaultText() {
 		mDateButton.setOnClickListener(new View.OnClickListener() {
-//			@Override
+			// @Override
 			public void onClick(View v) {
 				showDialog(DATE_PICKER_DIALOG);
 			}
 		});
 
 		mTimeButton.setOnClickListener(new View.OnClickListener() {
-//			@Override
+			// @Override
 			public void onClick(View v) {
 				showDialog(TIME_PICKER_DIALOG);
 			}
 		});
 
+		mConfirmButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				saveState();
+				setResult(RESULT_OK);
+				Toast.makeText(ReminderEditActivity.this,
+						getString(R.string.task_saved_message),
+						Toast.LENGTH_SHORT).show();
+				finish();
+			}
+		});
 		updateDateButtonText();
 		updateTimeButtonText();
 	}
 
+	public static final String DATE_TIME_FORMAT = "yyyy-MM-dd kk:mm:ss";
+
+	private void saveState() {
+		String title = mTitleText.getText().toString();
+		String body = mBodyText.getText().toString();
+		SimpleDateFormat dateTimeFormat = new SimpleDateFormat(DATE_TIME_FORMAT);
+		String reminderDateTime = dateTimeFormat.format(mCalendar.getTime());
+		long id = mDbHelper.createReminder(title, body, reminderDateTime);
+	}
+
 	private Calendar mCalendar = Calendar.getInstance();
-	private static final String TIME_FORMAT = "kk:mm";	
+	private static final String TIME_FORMAT = "kk:mm";
+
 	private void updateTimeButtonText() {
 		SimpleDateFormat timeFormat = new SimpleDateFormat(TIME_FORMAT);
 		String timeForButton = timeFormat.format(mCalendar.getTime());
 		mTimeButton.setText(timeForButton);
 	}
 
-	
 	private static final String DATE_FORMAT = "yyyy-MM-dd";
+
 	private void updateDateButtonText() {
 		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
 		String dateForButton = dateFormat.format(mCalendar.getTime());
@@ -85,12 +118,10 @@ public class ReminderEditActivity extends Activity {
 		return super.onCreateDialog(id);
 	}
 
-	
-	
 	private TimePickerDialog showTimePicker() {
 		TimePickerDialog timePicker = new TimePickerDialog(this,
 				new TimePickerDialog.OnTimeSetListener() {
-//					@Override 
+					// @Override
 					public void onTimeSet(TimePicker view, int hourOfDay,
 							int minute) {
 						mCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
@@ -100,15 +131,14 @@ public class ReminderEditActivity extends Activity {
 				}, mCalendar.get(Calendar.HOUR_OF_DAY),
 				mCalendar.get(Calendar.MINUTE), true);
 		return timePicker;
-	}	
-	
-	
+	}
+
 	private DatePickerDialog showDatePicker() {
 		// TODO Auto-generated method stub
 		DatePickerDialog datePicker = new DatePickerDialog(
 				ReminderEditActivity.this,
 				new DatePickerDialog.OnDateSetListener() {
-//					@Override
+					// @Override
 					public void onDateSet(DatePicker view, int year,
 							int monthOfYear, int dayOfMonth) {
 						mCalendar.set(Calendar.YEAR, year);
